ded85a6a6732cff2b39f1dab3da980492b0938e2
Propupul <luisvalencia39@hotmail.com>
2016-05-06 12:23:25 -0400

    Multiplication tables subgoal 2 was not met

---
+++ multiplication_table.py
@@ -0,0 +1,35 @@
+__author__ = "Propupul"
+
+
+the_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+              19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+              35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+              50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+              65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81]
+
+
+slicing = the_numbers[:9]
+for nums in slicing:
+    nums = (nums)
+    multiply = 2 * nums
+    multiply2 = 3 * nums
+    multiply3 = 4 * nums
+    multiply4 = 5 * nums
+    multiply5 = 6 * nums
+    multiply6 = 7 * nums
+    multiply7 = 8 * nums
+    multiply8 = 9 * nums
+    print(nums, end="        ")
+    print(multiply, end='        ')
+    print(multiply2, end='        ')
+    print(multiply3, end='        ')
+    print(multiply4, end='        ')
+    print(multiply5, end='        ')
+    print(multiply6, end='        ')
+    print(multiply7, end='        ')
+    print(multiply8, end='\n\n')
+
+
+
+
+
