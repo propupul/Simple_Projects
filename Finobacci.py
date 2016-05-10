ce729a7b55b9a1fc23b07734cc186fd66f477b86
Propupul <luisvalencia39@hotmail.com>
2016-05-10 14:45:43 -0400

    Fibonacci sequence, I printed the first 3 numbers outside the loop -3

---
+++ Finobacci.py
@@ -0,0 +1,41 @@
+__author__ = "Propupul"
+
+# BeginnerProjects Reddit
+# FIBONACCI Sequence
+
+
+def main():
+    number = int(input("Enter an integer: "))
+    fino = finobacci(number)
+
+    
+
+
+def finobacci(num):
+    a_list = list(range(num+1))
+    f0 = 0
+    f1 = 1
+    f2 = 1
+    print(f0)
+    print(f1)
+    print(f2)
+    result = 0
+    for number in a_list:
+        
+        adding = f1 + f2
+        result = adding + f2
+        f1 = adding
+        f2 = result
+        print(f1)
+        print(f2)
+        
+            
+        
+        
+        
+            
+            
+        
+
+
+main()
